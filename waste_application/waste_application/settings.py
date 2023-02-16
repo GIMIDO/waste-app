94a1533eac3d806e879ef02ff6c78580d631f1e2
GIMIDO <55269642+GIMIDO@users.noreply.github.com>
2023-02-16 13:09:59 +0300

    0.3

--- waste_application/waste_application/settings.py
+++ waste_application/waste_application/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = 'django-insecure--h7#$3lt(g46m@*hvy#+7@1sgtud)sk@zz2@nexr=d))s_9f$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ALLOWED_HOSTS = ['192.168.146.53']
 ALLOWED_HOSTS = ['*']
 
 # Application definition
